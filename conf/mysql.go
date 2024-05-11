6a5b9d10a50c2eb1e1e8f3c466b41fd46ac9d585
shihonghao <shihonghao2009@163.com>
2024-05-11 17:37:58 +0800

    init

--- conf/mysql.go
+++ conf/mysql.go
@@ -1,21 +1,21 @@
 package conf
+
 type DbConfig struct {
-	DriverName string
-	Dsn string
-	ShowSql bool
+	DriverName   string
+	Dsn          string
+	ShowSql      bool
 	ShowExecTime bool
-	MaxIdle int
-	MaxOpen int
+	MaxIdle      int
+	MaxOpen      int
 }
 
 var Db = map[string]DbConfig{
 	"db1": {
-		DriverName: "mysql",
-		Dsn:        "root:123456@tcp(127.0.0.1:3306)/ssodb?charset=utf8mb4&parseTime=true&loc=Local",
-		ShowSql:    true,
-		ShowExecTime:    false,
-		MaxIdle:    10,
-		MaxOpen:    200,
+		DriverName:   "mysql",
+		Dsn:          "root:096514@tcp([2409:8a00:c9b:87a1::75]:3306)/sso?charset=utf8mb4&parseTime=true&loc=Local",
+		ShowSql:      true,
+		ShowExecTime: false,
+		MaxIdle:      10,
+		MaxOpen:      200,
 	},
 }
-
--- conf/redis.go
+++ conf/redis.go
@@ -5,6 +5,6 @@ package conf
 var Redis = map[string]string{
 	"name":    "redis",
 	"type":    "tcp",
-	"address": "127.0.0.1:6379",
+	"address": "[2409:8a00:c9b:87a1::75]:6379",
 	"auth":    "",
 }
